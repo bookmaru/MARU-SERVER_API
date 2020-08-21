c792a0cc3b3461cd561dc2d55d50c54be9509ca9
최정균 <wjdrbs966@naver.com>
2020-08-21 19:50:42 +0900

    [UPDATE] Conflict Solved

--- MARU/routes/index.js
+++ MARU/routes/index.js
@@ -3,10 +3,6 @@ const router = express.Router();
 
 router.use('/search', require('./search'));
 router.use('/user', require('./user'));
-<<<<<<< HEAD
-//router.use('/chat', require('./chat'));
-=======
 router.use('/room', require('./room'));
->>>>>>> 1ebe36ae8023a394f0f39e2e48492460db6de3f6
 
 module.exports = router;
