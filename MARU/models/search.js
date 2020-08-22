f9db1c8c164afabeffb20210012c11c12400fe3d
최정균 <wjdrbs966@naver.com>
2020-08-22 12:33:29 +0900

    [UPDATE] 토론방 검색 쿼리

--- MARU/models/search.js
+++ MARU/models/search.js
@@ -1,8 +1,18 @@
 const pool = require('../modules/pool');
-const table = 'user';
+const table = 'room';
 
 const search = {
-
+  // 인원이 5명 미만인 채팅방만 나오는 쿼리
+  searchRoom : async (title) => {
+    const query = `SELECT * FROM ${table} WHERE title LIKE %${title}% GROUP BY roomIdx HAVING count(userIdx) < 5`;
+    try {
+      const result = await pool.queryParam(query);
+      return result;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
 }
 
 module.exports = search;
