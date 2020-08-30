8276aa26eff73982e5ed6a39d56309880ac22241
최정균 <wjdrbs966@naver.com>
2020-08-30 23:18:08 +0900

    [TEST] 검색

--- MARU/controllers/searchController.js
+++ MARU/controllers/searchController.js
@@ -17,9 +17,9 @@ const search = {
     const { title } = req.query;
     console.log(title);
 
-    const kakao = await kakaoOptions.kakaoTest(encodedStr);
+    const kakao = await kakaoOptions.kakaoTest(title);
 
-    if (!title || !encodedStr) {
+    if (!title) {
       res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
       return;
     }
