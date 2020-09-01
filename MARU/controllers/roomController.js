d700b99828a7f09a4696fa228292e338ab014f47
yez <yejison98@gmail.com>
2020-09-01 22:34:36 +0900

    [UPDATE] 코드정리

--- MARU/controllers/roomController.js
+++ MARU/controllers/roomController.js
@@ -96,9 +96,9 @@ const room = {
 
       
     },
-    mainRoom: async(req, res)=>{
+    mainRoom: async (req, res) => { 
       const roomIdx = req.params.roomIdx;
-      if( !roomIdx ){
+      if (!roomIdx) {
           res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
           return;
       }
@@ -106,19 +106,19 @@ const room = {
       return res.status(statusCode.OK)
           .send(util.success(statusCode.OK, resMessage.READ_ROOM_SUCCESS, idx));
   }, 
-  quizRoom: async(req, res) =>{
+  quizRoom: async (req, res) => {
       const roomIdx = req.params.roomIdx;
-      if( !roomIdx ) {
+      if (!roomIdx) {
           res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
           return;
       }
       const userIdx = req.userIdx;
-      if( !userIdx ) {
+      if (!userIdx) {
           res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.EMPTY_TOKEN));
           return;
       }
       const result = await roomModel.quizRoom(userIdx,roomIdx);
-      if( result.length === 0 ) {
+      if (result.length === 0) {
           res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.NO_ROOM));
       }
     }
--- MARU/models/room.js
+++ MARU/models/room.js
@@ -59,16 +59,16 @@ const room = {
       throw err;
     }
   },
-  mainRoom: async(roomIdx) => {
+  mainRoom: async (roomIdx) => {
     const query = `SELECT r.thumbnail, r.title, r.authors, r.createdAt, r.info, u.nickName,
     round((u.rating/u.count),1) as avgRating, 
     (SELECT COUNT(chat.userIdx) - 1 FROM chat WHERE chat.roomIdx = "${roomIdx}") as peopleCount
     FROM room r JOIN user u ON (r.userIdx = u.userIdx)
     WHERE r.roomIdx = "${roomIdx}"`;
-    try{
+    try {
         const result = await pool.queryParamArr(query);
         return result;
-    } catch(err) {
+    } catch (err) {
         if (err.errno == 1062) {
             console.log('duplicate ERROR : ', err.errno, err.code);
             throw err;
@@ -77,13 +77,13 @@ const room = {
         throw err;
     }  
 },
-quizRoom: async(userIdx, roomIdx)=>{
+quizRoom: async (userIdx, roomIdx) => {
     const query = `SELECT room.quiz1, room.quiz2, room.quiz3, room.quiz4, room.quiz5, room.answer1, room.answer2, room.answer3, room.answer4, room.answer5 
     FROM room WHERE room.userIdx = "${userIdx}" AND room.roomIdx = "${roomIdx}"`;
-    try{
+    try {
         const result = await pool.queryParam(query);
         return result;
-    } catch(err){
+    } catch (err) {
         if( err.errno ==1062 ) {
             console.log('duplicate ERROR : ', err.errno, err.code);
             throw err;
