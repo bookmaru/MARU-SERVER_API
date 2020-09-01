7e614d6cd01b6ed1408ed954485f96bcb00afddd
최정균 <wjdrbs966@naver.com>
2020-09-01 17:33:50 +0900

    [UPDATE] code convention

--- MARU/controllers/roomController.js
+++ MARU/controllers/roomController.js
@@ -94,13 +94,12 @@ const room = {
       }
 
       // 토론방 참여 불가능
-      res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NOT_POSSIBLE_JOIN_ROOM));
-
-      
+      res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NOT_POSSIBLE_JOIN_ROOM));  
     },
-    mainRoom: async(req, res)=>{
+
+    mainRoom: async (req, res) => {
       const roomIdx = req.params.roomIdx;
-      if( !roomIdx ){
+      if (!roomIdx) {
           res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
           return;
       }
@@ -108,19 +107,20 @@ const room = {
       return res.status(statusCode.OK)
           .send(util.success(statusCode.OK, resMessage.READ_ROOM_SUCCESS, idx));
   }, 
-  quizRoom: async(req, res) =>{
+
+  quizRoom: async (req, res) => {
       const roomIdx = req.params.roomIdx;
-      if( !roomIdx ) {
+      if (!roomIdx) {
           res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
           return;
       }
       const userIdx = req.userIdx;
-      if( !userIdx ) {
+      if (!userIdx) {
           res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.EMPTY_TOKEN));
           return;
       }
       const result = await roomModel.quizRoom(userIdx,roomIdx);
-      if( result.length === 0 ) {
+      if (result.length === 0) {
           res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.NO_ROOM));
       }
     },
