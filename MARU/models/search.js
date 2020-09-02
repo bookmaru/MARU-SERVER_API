5622c7cebe3fc04e394ca7edded7f03c7ca75c47
최정균 <wjdrbs966@naver.com>
2020-09-02 14:42:56 +0900

    [UPDATE] 개설된 토론방 roomIdx 추가

--- MARU/models/search.js
+++ MARU/models/search.js
@@ -4,7 +4,7 @@ const table = 'room';
 const search = {
   // 인원이 5명 미만인 채팅방만 나오는 쿼리
   searchRoom : async (title) => {
-    const query = `SELECT r.thumbnail, r.title, r.authors, r.info, u.nickName FROM ${table} r JOIN user u ON r.userIdx = u.userIdx WHERE r.title LIKE '%${title}%' GROUP BY r.roomIdx HAVING count(r.userIdx) < 5`;
+    const query = `SELECT r.roomIdx, r.thumbnail, r.title, r.authors, r.info, u.nickName FROM ${table} r JOIN user u ON r.userIdx = u.userIdx WHERE r.title LIKE '%${title}%' GROUP BY r.roomIdx HAVING count(r.userIdx) < 5`;
     try {
       const result = await pool.queryParam(query);
       return result;
