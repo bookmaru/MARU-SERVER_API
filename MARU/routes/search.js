ae585b02e1ca8b2661195bc06bfec4805fabeedb
최정균 <wjdrbs966@naver.com>
2020-09-03 01:55:41 +0900

    [UPDATE] 모임 검색

--- MARU/routes/search.js
+++ MARU/routes/search.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/searchController');
+const middleware = require('../modules/middlewares');
 
 router.get('/book', searchController.kakao);
 router.get('/room', searchController.room);
