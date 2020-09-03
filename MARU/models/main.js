f6c784221ed79ced56db4e9a37cb63ef4124e3f8
최정균 <wjdrbs966@naver.com>
2020-09-03 18:58:43 +0900

    [UPDATE] 메인화면 쿼리리

--- MARU/models/main.js
+++ MARU/models/main.js
@@ -4,27 +4,30 @@ const table = 'room';
 const main = {
   // 모임이 많은 책
   ManyRoom: async () => {
-    const query = `SELECT roomIdx, thumbnail, authors, title, COUNT(*) as roomCount FROM ${table} GROUP BY title ORDER BY roomCount DESC LIMIT 0, 9`; 
+    const query = `SELECT thumbnail, title, authors, count(title) as roomCount from room GROUP BY title ORDER BY roomCount DESC LIMIT 0, 10`; 
     try {
       const result = await pool.queryParam(query);
       return result;
     } catch (err) {
       console.log(err);
+      throw err;
     }
   },
 
   // 새로나온 모임 
   newRoom: async(pageStart, pageEnd) => {
-    const query = `SELECT r.thumbnail, r.authors, r.title, r.info, u.nickName FROM room r JOIN user u ON r.userIdx = u.userIdx ORDER BY roomIdx DESC LIMIT ${pageStart}, ${pageEnd}`;
+    const query = `SELECT r.roomIdx, r.thumbnail, r.authors, r.title, r.info, u.nickName FROM room r JOIN user u ON r.userIdx = u.userIdx ORDER BY roomIdx DESC LIMIT ${pageStart}, ${pageEnd}`;
     try {
       const result = await pool.queryParam(query);
       return result;
     } catch (err) {
       console.log(err);
-    }
+      throw err;
+    } 
   }
-  
 }
 
 module.exports = main;
+
+
 
