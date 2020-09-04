ba43d35c96d1cc44a0b2c9459913ce7649ae7361
최정균 <wjdrbs966@naver.com>
2020-09-05 01:12:13 +0900

    [UPDATE] 모임 채팅불러오기

--- MARU/controllers/chatController.js
+++ MARU/controllers/chatController.js
@@ -2,14 +2,24 @@ const util = require('../modules/util');
 const statusCode = require('../modules/statusCode');
 const resMessage = require('../modules/responseMessage');
 const chatModel = require('../models/chat');
+
 const chat = {
   getChat: async (req, res) => {
     const roomIdx = req.params.roomIdx;
-    const getChat = await chatModel.getChat(roomIdx);
-    if (getChat) {
-      res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.POSSIBLE_JOIN_ROOM, getChat));
+    
+    if (!roomIdx) {
+      res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
       return;
     }
+
+    try {
+      const getChat = await chatModel.getChat(roomIdx);
+      res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.POSSIBLE_JOIN_ROOM, getChat));
+      return;
+    } catch (err) {
+      res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.SERVER_ERROR));
+    }
+  
   }
 }
 
