771408e913c59e8b5675091018ae558f9aa4aba0
최정균 <wjdrbs966@naver.com>
2020-09-18 23:09:10 +0900

    [UPDATE] 검색 쿼리

--- MARU/models/search.js
+++ MARU/models/search.js
@@ -7,7 +7,6 @@ const search = {
     const query = `SELECT r.roomIdx, r.thumbnail, r.title, r.authors, r.info, r.createdAt, u.nickName
                    FROM room r JOIN user u ON r.userIdx = u.userIdx 
                    JOIN participant p ON p.roomIdx = r.roomIdx 
-                   JOIN quizFail q 
                    WHERE r.expired = 'false' and r.consonantVowel LIKE '%${title}%' and r.roomIdx not in (select roomIdx from quizFail where userIdx = ${userIdx}) and r.roomIdx not in (select roomIdx from participant where userIdx = ${userIdx})
                    GROUP BY r.roomIdx HAVING count(p.userIdx) < 5`;
     try {
