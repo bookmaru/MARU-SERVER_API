a36a6679511be4c927d0b6de9864efcb0bad0bb1
최정균 <wjdrbs966@naver.com>
2020-09-20 16:40:12 +0900

    [FIX] 방만들기

--- MARU/controllers/roomController.js
+++ MARU/controllers/roomController.js
@@ -40,7 +40,7 @@ const room = {
       // 자모음 분리 결과가 배열이라 문자열로 합치기 
       const titleConsonatVowel = consonantVowel.join("");
 
-      const roomMake = await roomModel.make(thumbnail, authors, title, titleConsonatVowel, info, quiz1, quiz2, quiz3, quiz4, quiz5, answer1, answer2, answer3, answer4, answer5, createdAt, userIdx, expired);
+      const roomMake = await roomModel.make(thumbnail, authors, title, titleConsonatVowel, info, quiz1, quiz2, quiz3, quiz4, quiz5, answer1, answer2, answer3, answer4, answer5, createdAt, userIdx);
       const participantAdd = await roomModel.addUser(userIdx, roomMake);
       res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.MAKE_ROOM_SUCCESS, {
         roomIdx: roomMake
